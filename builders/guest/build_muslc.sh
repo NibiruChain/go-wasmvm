2e7ad34602f289e44cc13113f13c25a961427eb8
Unique Divine <realuniquedivine@gmail.com>
2026-06-23 18:17:21 -0500

    fix(wasmvm): Scope musl cross compiler to aarch64 builds

--- builders/guest/build_muslc.sh
+++ builders/guest/build_muslc.sh
@@ -7,9 +7,9 @@ export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 # enable stripping through cargo (if that is desired).
 
 echo "Starting aarch64-unknown-linux-musl build"
-export CC=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+export CC_aarch64_unknown_linux_musl=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
 cargo build --release --target aarch64-unknown-linux-musl --example wasmvmstatic
-unset CC
 
 echo "Starting x86_64-unknown-linux-musl build"
 cargo build --release --target x86_64-unknown-linux-musl --example wasmvmstatic
